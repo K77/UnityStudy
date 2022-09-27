a57ed85f9a1d448d22e9ce42534ebf8e911f9951
K <>
2022-09-27 20:40:18 +0800

    动画融合相关

--- Assets/_Study/0__Other/01_Playable/TestPlayable.cs
+++ Assets/_Study/0__Other/01_Playable/TestPlayable.cs
@@ -7,33 +7,89 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.Playables;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Animator))]
 public class TestPlayable : MonoBehaviour
 {
-    public AnimationClip clip0;
-    public AnimationClip clip1;
+    public Slider _SliderHeight;
+    public Slider _SliderSpeed;
+    public AnimationClip clipIdleH;
+    public AnimationClip clipIdleM;
+    public AnimationClip clipIdleL;
+    public AnimationClip clipWalkH;
+    public AnimationClip clipWalkM;
+    public AnimationClip clipWalkL;
+    public AnimationClip clipCrabH;
+    public AnimationClip clipCrabM;
+    public AnimationClip clipCrabL;
+    
     private PlayableGraph m_Graph;
     private AnimationPlayableOutput m_Output;
-    private AnimationMixerPlayable m_Mixer;
+    private AnimationMixerPlayable m_MixerIdle;
+    private AnimationMixerPlayable m_MixerWalk;
+    private AnimationMixerPlayable m_MixerAll;
 
     [Range(0, 1)] public float weight;
     void Start()
     {
         // PlayableBehaviour
-        m_Graph = PlayableGraph.Create("asdfas");
-        m_Mixer = AnimationMixerPlayable.Create(m_Graph, 2);
+        m_Graph = PlayableGraph.Create("Graph");
+        m_MixerIdle = AnimationMixerPlayable.Create(m_Graph, 2);
+        m_MixerWalk = AnimationMixerPlayable.Create(m_Graph, 2);
+        m_MixerAll = AnimationMixerPlayable.Create(m_Graph, 2);
         m_Output = AnimationPlayableOutput.Create(m_Graph, "Animation", GetComponent<Animator>());
 
-        m_Output.SetSourcePlayable(m_Mixer);
-        AnimationClipPlayable clipPlayable0 = AnimationClipPlayable.Create(m_Graph, clip0);
-        AnimationClipPlayable clipPlayable1 = AnimationClipPlayable.Create(m_Graph, clip1);
-        m_Graph.Connect(clipPlayable0, 0, m_Mixer, 0);
-        m_Graph.Connect(clipPlayable1, 0, m_Mixer, 1);
-        m_Mixer.SetInputWeight(0, 0.5f);
-        m_Mixer.SetInputWeight(1, 0.5f);
-        m_Graph.Play();
+        AnimationClipPlayable clipPlayable0 = AnimationClipPlayable.Create(m_Graph, clipIdleH);
+        AnimationClipPlayable clipPlayable1 = AnimationClipPlayable.Create(m_Graph, clipIdleM);
+        AnimationClipPlayable clipPlayable2 = AnimationClipPlayable.Create(m_Graph, clipWalkH);
+        AnimationClipPlayable clipPlayable3 = AnimationClipPlayable.Create(m_Graph, clipWalkM);
+        
+        // m_ParentMixerPlayable.AddInput(m_IdleMixerPlayable, 0, 1);
+        // m_ParentMixerPlayable.AddInput(m_MoveMixerPlayable, 0, 0);
+        
+        m_Graph.Connect(clipPlayable0, 0, m_MixerIdle, 0);
+        m_Graph.Connect(clipPlayable1, 0, m_MixerIdle, 1);
+        m_Graph.Connect(clipPlayable2, 0, m_MixerWalk, 0);
+        m_Graph.Connect(clipPlayable3, 0, m_MixerWalk, 1);
+        
+        m_MixerAll.AddInput(m_MixerIdle,0,0);
+        m_MixerAll.AddInput(m_MixerWalk,0,1);
+
+        // m_Mixer.SetInputWeight(0, 0.5f);
+        // m_Mixer.SetInputWeight(1, 0.5f);
+        // clipIdleH.isLooping = true;
         // GameObject.Instantiate(gameObject, transform.parent);
+
+        _SliderHeight.value = 1;
+        _SliderHeight.onValueChanged.AddListener((value) =>
+        {
+            changeBlend();
+        });
+        
+        _SliderSpeed.onValueChanged.AddListener((value) =>
+        {
+            changeBlend();
+            Debug.Log(_SliderSpeed.value);
+        });
+        m_MixerAll.SetInputWeight(m_MixerIdle,0);
+        m_MixerAll.SetInputWeight(m_MixerWalk,1);
+        m_MixerWalk.SetInputWeight(0,1);
+        m_MixerWalk.SetInputWeight(1,0);
+        m_Output.SetSourcePlayable(m_MixerAll);
+        m_Graph.Play();
+    }
+
+    void changeBlend()
+    {
+        m_MixerIdle.SetInputWeight(0, _SliderHeight.value);
+        m_MixerIdle.SetInputWeight(1, 1 - _SliderHeight.value);
+        m_MixerWalk.SetInputWeight(0, _SliderHeight.value);
+        m_MixerWalk.SetInputWeight(1, 1 - _SliderHeight.value);
+        
+        m_MixerAll.SetInputWeight(m_MixerIdle,1- _SliderSpeed.value);
+        m_MixerAll.SetInputWeight(m_MixerWalk,_SliderSpeed.value);
     }
 
     private void OnGUI()
@@ -49,7 +105,7 @@ public class TestPlayable : MonoBehaviour
 
     void Update()
     {
-        m_Mixer.SetInputWeight(0, weight);
-        m_Mixer.SetInputWeight(1, 1 - weight);
+        // m_Mixer.SetInputWeight(0, weight);
+        // m_Mixer.SetInputWeight(1, 1 - weight);
     }
 }
