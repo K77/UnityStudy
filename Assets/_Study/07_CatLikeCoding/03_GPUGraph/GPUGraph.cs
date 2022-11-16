d6ace133236a77b01ca936bc72cd81021f7042df
K <>
2022-11-16 19:56:17 +0800

    分形相关

--- Assets/_Study/07_CatLikeCoding/03_GPUGraph/GPUGraph.cs
+++ Assets/_Study/07_CatLikeCoding/03_GPUGraph/GPUGraph.cs
@@ -2,11 +2,22 @@ using UnityEngine;
 
 public class GPUGraph : MonoBehaviour {
 
-    //[SerializeField]
-    //Transform pointPrefab;
+    static readonly int
+        positionsId = Shader.PropertyToID("_Positions"),
+        resolutionId = Shader.PropertyToID("_Resolution"),
+        stepId = Shader.PropertyToID("_Step"),
+        timeId = Shader.PropertyToID("_Time");
 
     [SerializeField, Range(10, 200)]
     int resolution = 10;
+    [SerializeField]
+    ComputeShader computeShader;
+    
+    [SerializeField]
+    Material material;
+
+    [SerializeField]
+    Mesh mesh;
 
     [SerializeField]
     FunctionLibrary.FunctionName function;
@@ -39,12 +50,24 @@ public class GPUGraph : MonoBehaviour {
     }
     void Update()
     {
-        
+        UpdateFunctionOnGPU();
     }
 
     void PickNextFunction()
     {
         
+    }
+    
+    void UpdateFunctionOnGPU () {
+        float step = 2f / resolution;
+        computeShader.SetInt(resolutionId, resolution);
+        computeShader.SetFloat(stepId, step);
+        computeShader.SetFloat(timeId, Time.time);
+        computeShader.SetBuffer(0, positionsId, positionsBuffer);
+        int groups = Mathf.CeilToInt(resolution / 8f);
+        computeShader.Dispatch(0, groups, groups, 1);
+        var bounds = new Bounds(Vector3.zero, Vector3.one * 2f);
+        Graphics.DrawMeshInstancedProcedural(mesh, 0, material, bounds, positionsBuffer.count);
     }
 
     //void UpdateFunction () { … }
